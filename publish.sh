3873319bd3c1deb551f85b22a607e14f99e687b1
Roc Deng <rocdeng@gmail.com>
2026-07-12 16:31:59 +0800

    change sh

--- publish.sh
+++ publish.sh
@@ -1,2 +1,9 @@
+TAG=$(git rev-parse --short HEAD)
+
 docker compose build
+docker tag baidudisklink:latest 192.168.1.5:35000/baidudisklink:$TAG
+docker tag baidudisklink:latest 192.168.1.5:35000/baidudisklink:latest
+
+docker push 192.168.1.5:35000/baidudisklink:$TAG
 docker push 192.168.1.5:35000/baidudisklink:latest
+
